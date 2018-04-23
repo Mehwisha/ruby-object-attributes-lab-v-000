636d6fa76579bf7af79d6ad24fd00565ebe405df
Mehwish Aziz <mehwisha@msn.com>
2018-04-23 01:46:38 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,17 @@
-class Dog
-
-def name
-@name
-end
-def name=(new_name)
-@name =  name
-end
-def breed
-@breed
-end
-def breed=(new_breed)
-@breed = breed
-end
-end
-fido = Dog.new("fido")
-Shephard = Dog.new("Shephard")
+# class Dog
+#
+# def name
+# @name
+# end
+# def name=(new_name)
+# @name =  name
+# end
+# def breed
+# @breed
+# end
+# def breed=(new_breed)
+# @breed = breed
+# end
+# end
+# fido = Dog.new("fido")
+# Shephard = Dog.new("Shephard")
