c990ac4f78c5a9a535496d6201f5c1921eca0a62
Mehwish Aziz <mehwisha@msn.com>
2018-04-23 01:39:48 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,23 +2,14 @@ class Dog
 
 def name
 @name
-
 end
-
-def name =(name)
+def name = (name)
 @name =  name
-
 end
-
 def breed
 @breed
 end
 def breed = (breed)
 @breed = breed
 end
-end
-
-
-
-
 end
